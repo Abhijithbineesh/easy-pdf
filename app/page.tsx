bee60dd8447ec7306ac3e8918cd4cb51c3778902
Abhijithbineesh <abhijith.kpbineesh@gmail.com>
2025-04-10 22:37:54 +0530

    demosection completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import DemoSection from "@/components/home/demoSection";
 import HeroSection from "@/components/home/heroSection";
 
 export default function Home() {
   return <div className="relative w-full">
     <div className="flex flex-col">
     <HeroSection/>
+    <DemoSection/>
     </div>
     </div>;
 }

4fccc0174c0154caf671b5a9d9f934b6b2c262d3
Abhijithbineesh <abhijith.kpbineesh@gmail.com>
2025-04-11 12:31:06 +0530

    completed how-it-works.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import DemoSection from "@/components/home/demoSection";
 import HeroSection from "@/components/home/heroSection";
+import HowItWorks from "@/components/home/howitWorks";
 
 export default function Home() {
   return <div className="relative w-full">
     <div className="flex flex-col">
     <HeroSection/>
     <DemoSection/>
+    <HowItWorks/>
     </div>
     </div>;
 }
---
+++ components/home/howitWorks.tsx
@@ -0,0 +1,69 @@
+import { BrainCircuit, DockIcon, FileAxis3d } from "lucide-react";
+import { ReactNode } from "react";
+
+export default function HowItWorks() {
+  type Step = {
+    icon: ReactNode;
+    label: string;
+    description: string;
+  };
+
+  const steps: Step[] = [
+    {
+      icon: <DockIcon size={64} strokeWidth={1.5} />,
+      label: "Upload your pdf",
+      description: "Upload your pdf file to get started",
+    },
+
+    {
+      icon: <BrainCircuit size={64} strokeWidth={1.5} />,
+      label: "AI Analysis",
+      description:
+        "Our advanced AI processes and analyzes your document instantly",
+    },
+
+    {
+      icon: <FileAxis3d size={64} strokeWidth={1.5} />,
+      label: "Get Summary",
+      description: "Receive a clear,concide summary of your Document",
+    },
+  ];
+
+  return (
+    <section className="relative overflow-hidden">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        <div className="text-center mb-16">
+          <h2 className="text-blue-600 font-bold text-xl uppercase mb-4 ">
+            HOW IT WORKS
+          </h2>
+          <h3 className="font-bold text-3xl max-w-2xl mx-auto">
+            Transform any PDF into an easy-to-digest summary in three simple
+            steps
+          </h3>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl  mx-auto relative">
+          {steps.map((step, idx) => (
+            <StepItem key={idx} {...step} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function StepItem({ icon, label, description }: Step) {
+  return (
+    <div className="relative p-6 rounded-2xl bg-white/5 backdrop-blur-xs border border-white/10 hover:border-blue-600 transition-colors group-w-full">
+      <div className="flex flex-col gap-4 h-full ">
+        <div className="flex items-center justify-center h-24 w-24 mx-auto rounded-2xl bg-linear-to-br from-blue-600/10 to-transparent group-hover:from-blue-600/20 transition-colors">
+          <div className="text-blue-600">{icon}</div>
+        </div>
+        <div className="flex flex-col flex-1 gap-1 justify-between ">
+          <h4 className="text-center font-bold text-xl">{label}</h4>
+          <p className="text-center text-gray-600 text-sm">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
